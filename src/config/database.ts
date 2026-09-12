cb1c0d5f2093152853af6ca1bd49f5670d345593
Victor <victorcriri313@gmail.com>
2026-09-12 15:34:46 -0300

    fix: configure DNS servers for MongoDB connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import * as dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 export async function connectDatabase(): Promise<void> {
     await mongoose.connect(process.env.MONGO_URI!);
